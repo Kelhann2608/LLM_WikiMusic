8f768c177a67ad24c7d251de54d34c9991f3549a
amandine <constans.amandine@gmail.com>
2021-11-26 00:50:43 +0100

    modified Artist.jsx & conflict

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/genre/:tag" element={<ArtistList />} />
-          <Route path="genre/:tag/artist/:artistName" element={<Artist />} />
+          <Route path="/genre/:tag/artist/:artistName" element={<Artist />} />
         </Routes>
       </Router>
 
