345ba39874b49868ce220741df5c9c71d55d5905
Louya64 <cixie@live.fr>
2021-11-25 17:11:16 +0100

    routes to artist and tracklist

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import Home from "./components/home/Home";
 import ArtistList from "./components/artistList/ArtistList";
 import Artist from "./components/artist/Artist";
 import Track from "./components/track/Track";
-import './app.css';
+import "./app.css";
 
-function App() { 
+function App() {
   return (
     <div className="App">
-      
-    <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/genre/:tag" element={<ArtistList />} />
-        {/* <Route path="/artist/:artistName" element={<Artist />} />
-        <Route path="/tracks/:trackName" element={<Track />} /> */}
-      </Routes>
-    </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/genre/:tag" element={<ArtistList />} />
+          <Route path="genre/:tag/artist/:artistName" element={<Artist />} />
+          <Route
+            path="genre/:tag/artist/:artistName/track/:trackName"
+            element={<Track />}
+          />
+        </Routes>
+      </Router>
 
-
-     <h1>Elle, elle aime ! dans app</h1>
+      <h1>Elle, elle aime ! dans app</h1>
     </div>
   );
 }
