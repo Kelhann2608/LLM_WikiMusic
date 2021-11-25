c6b637d9432943895fa22e51beca71cee5428802
Laurent SLONIM <slonim.laurent@gmail.com>
2021-11-25 18:26:29 +0100

    add artistID to routes

--- src/components/cards/ArtistCard.jsx
+++ src/components/cards/ArtistCard.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 
 const ArtistCard = ({name, img, tag}) => {
    const [picture, setPicture] = useState()
+   const [artistID, setArtistID] = useState()
    console.log(name, 'NAMEEEE')
     const apiKey = "api_key=edabdc8efa6ff44658d08a93a343cf21"
     //setPicture(res.data.artist.image[1]['#text'])
@@ -14,14 +15,18 @@ const ArtistCard = ({name, img, tag}) => {
         
        name && axios    
         .get(`https://theaudiodb.com/api/v1/json/1/search.php?s=${name}`)
-        .then((res) => setPicture(res.data.artists[0].strArtistBanner))
+        .then((res) => {
+            setPicture(res.data.artists[0].strArtistBanner)
+            setArtistID(res.data.artists[0].idArtist)
+
+        } )
         .catch((err)=> console.log(err))
     },[]);
 console.log(img)
     // link ver "/genre/:tag />
    
     return (
-        <Link to={`/genre/${tag}/artist/${name}`}>
+        <Link to={`/genre/${tag}/artist/${artistID}`}>
             <div className="artist-card">
                 <img src={picture}/>
                 <h4>{name}</h4>
