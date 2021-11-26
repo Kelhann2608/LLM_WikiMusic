cac8d297f5e50d0742312b1573d937b0e77efc5a
Louya64 <cixie@live.fr>
2021-11-26 12:04:15 +0100

    fix bug home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,13 +3,13 @@ import TagCard from "../cards/TagCard";
 import axios from "axios";
 import "./home.css";
 import vinyl from "../../assets/vinyl.png";
-import loupeSearch from "../../assets/search.png"
+import loupeSearch from "../../assets/search.png";
 
 const Home = () => {
   const apiKey = "api_key=edabdc8efa6ff44658d08a93a343cf21";
   const [tags, setTags] = useState();
   const [loading, setLoading] = useState(true);
-  const [searchTag, setSearchTag] = useState('');
+  const [searchTag, setSearchTag] = useState("");
 
   useEffect(() => {
     setTimeout(
@@ -29,22 +29,29 @@ const Home = () => {
 
   return (
     <>
-<<<<<<< HEAD
-     <div className="inputSearch"> <label ><img src={loupeSearch} width="18px"/></label>
+      <div className="inputSearch">
+        {" "}
+        <label>
+          <img src={loupeSearch} width="18px" />
+        </label>
         <input
           className="searchInput"
           type="text"
           placeholder="rechercher un thème"
           value={searchTag}
           onChange={(e) => setSearchTag(e.target.value.toLowerCase())}
-        /></div>
-      {loading ? <div className="vinylContainer"><img className="vinyl" src={vinyl} height="300" width="300" /></div> : null}
-=======
-      {loading ? <div className="vinylContainer"><img className="vinyl" src={vinyl} alt="" height="300" width="300" /></div> : null}
->>>>>>> dev
+        />
+      </div>
+      {loading ? (
+        <div className="vinylContainer">
+          <img className="vinyl" src={vinyl} height="300" width="300" />
+        </div>
+      ) : null}
       <div className="cardsContainer">
         {tags &&
-          tags.filter((tag) => tag.name.toLowerCase().includes(searchTag)).map((tag, index) => <TagCard key={index} name={tag.name} />)}
+          tags
+            .filter((tag) => tag.name.toLowerCase().includes(searchTag))
+            .map((tag, index) => <TagCard key={index} name={tag.name} />)}
       </div>
     </>
   );
