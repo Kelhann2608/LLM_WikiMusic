d563aeb04f69810f1d5e2fd0f138b4865eebd257
Louya64 <cixie@live.fr>
2021-11-28 14:37:54 +0100

    add basename to router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "./app.css";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/LLM_WikiMusic/">
         <Routes>
           <Route exact path="/" element={<Welcome />} />
           <Route path="/genres" element={<><Header /><Home /></>} />
